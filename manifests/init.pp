8fe70cff54dedae646cb503081e58de282daf7bd
Marc Qualie <marc@marcqualie.com>
2015-03-02 13:55:30 +0000

    New url for xquartz download

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,6 +8,6 @@ class xquartz (
 ) {
   package { 'XQuartz':
     provider => 'pkgdmg',
-    source   => "http://xquartz.macosforge.org/downloads/SL/XQuartz-${version}.dmg",
+    source   => "http://download2us.softpedia.com/dl/ea1afe7bb4e6d04505164f6fc5a1ffe1/54f4695b/400030084/mac/System-Utilities/XQuartz-${version}.dmg",
   }
 }
